be52fe9d4fdd6a4ac7523bdd4acf75929dac8a22
Artyouha <Artyouha@yandex.ru>
2023-02-09 01:14:44 +0200

    old

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,31 +1,37 @@
-class Game  {
+class Game {
   constructor(container) {
     this.container = container;
-    this.wordElement = container.querySelector ('.word');
-    this.winsElement = container.querySelector ('.status__wins');
-    this.lossElement = container.querySelector ('.status__loss');
+    this.wordElement = container.querySelector('.word');
+    this.winsElement = container.querySelector('.status__wins');
+    this.lossElement = container.querySelector('.status__loss');
+
     this.reset();
 
     this.registerEvents();
   }
 
-  reset () {
+  reset() {
+    this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
-    this.setNewWord();
   }
 
   registerEvents() {
-    document.addEventListener('keydown', (event) => {
-      let symbol = this.currentSymbol;
-      if (symbol.textContent === event.key) {
-        this.success();
+    let index = 0;
+    const that = this;
+    document.onkeypress = function(event) {
+      const symbol = document.body.querySelectorAll(".symbol");
+      let arr = Array.from(symbol);
+      if (arr[index].textContent == event.key.toLowerCase()) {
+        index += 1;
+        that.success();
+        if (index  == (arr.length)){
+          index = 0;
+        }
+      } else {
+        that.fail()
       }
-      else {
-        this.fail();
-      }
-    });
-
+    }
   }
 
   success() {
@@ -58,56 +64,33 @@ class Game  {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
-
     return words[index];
   }
 
   renderWord(word) {
-    this.wordElement.innerHTML = [...word]
-        .map(
-            (s, i) =>
-                `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
-        )
-        .join('');
+    const html = [...word]
+      .map(
+        (s, i) =>
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
+      )
+      .join('');
+    this.wordElement.innerHTML = html;
 
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
-
-    let string = Array.from(document.querySelectorAll('div.word span.symbol'));
-    this.countdown = string.length;
-    this.countdownElement = countdownElement;
-    this.countdownElement.textContent = string.length.toString();
-    this.counter();
-  }
-
-  counter() {
-    clearInterval(interval);
-    interval = setInterval(() => {
-      if (this.countdown > 0) {
-        this.countdown -= 1;
-        this.countdownElement.textContent = this.countdown;
-      }
-      else {
-        this.fail();
-      }
-    }, 1000);
   }
 }
 
-let countdownElement = document.getElementById('timer');
-let countdown = parseInt(countdownElement.textContent);
-let interval;
-
-new Game(document.getElementById('game'));
+new Game(document.getElementById('game'))
