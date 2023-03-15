76f0336960d20e38a155b4cf0d872be7f782ba6f
Artyouha <Artyouha@yandex.ru>
2023-03-15 10:43:42 +0200

    New4

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,33 +1,32 @@
 const progress = document.getElementById( 'progress' );
 
-const form = document.getElementById('form');
+document.forms.upload.onsubmit = function() {
+    let input = this.elements.file;
+    let file = input.files[0];
+    if (file) {
+        upload(file);
+    }
+    return false;
+}
 
-form.addEventListener('submit', (e) => {
+function upload(file) {
 
-  e.preventDefault();
+    let xhr = new XMLHttpRequest();  
 
-    const formData = new FormData(form);
-    
-
-    
-});
-
-function fn(){ addEventListener('progress', (e) => {
-        const xhr = new XMLHttpRequest();
-    xhr.upload.onprogress = function(e) {
-        alert(`Отправлено ${e.loaded} из ${e.total}`);
-      };
-    
-      xhr.onloadend = function() {
-        if (xhr.status == 200) {
-          alert("Успех");
-        } else {
-          alert("Ошибка " + this.status);
+    xhr.upload.onprogress = function(event) {
+        progress.value = event.loaded/event.total;
+    }
+  
+    xhr.onload = xhr.onerror = function() {
+        if (this.status == 200) {
+            log("success");
         }
-      };
-    ;
-    xhr.open("POST", 'https://students.netoservices.ru/nestjs-backend/upload');
-    addListeners(xhr)
-    xhr.send(formData);
-});
+        else {
+            log("error " + this.status);
+        }
+    };
+  
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload", true);
+    xhr.send(file);
+  
 }
